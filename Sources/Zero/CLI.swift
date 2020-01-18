ba533650c475b45427d05076635a33c91084faba
Michael Sanders <michael.sanders@fastmail.com>
2020-01-17 19:03:28 -0800

    Add custom error message formatter

--- Sources/Zero/CLI.swift
+++ Sources/Zero/CLI.swift
@@ -2,17 +2,27 @@ import Foundation
 import SwiftCLI
 
 public enum Zero {
-    public static let cli: CLI = .init(
-        name: "zero",
-        version: "0.2.0-beta",
-        description: "Radically simple personal bootstrapping tool for macOS.",
-        commands: [
-          SetupCommand(),
-          UpdateCommand(),
-          BundleCommand(),
-          ApplyDefaultsCommand(),
-          ApplySymlinksCommand(),
-          RunScriptsCommand(),
-        ]
-    )
+    public static let cli: CLI = {
+        let cli = CLI(
+            name: "zero",
+            version: "0.2.0-beta",
+            description: "Radically simple personal bootstrapping tool for macOS.",
+            commands: [
+                SetupCommand(),
+                UpdateCommand(),
+                BundleCommand(),
+                ApplyDefaultsCommand(),
+                ApplySymlinksCommand(),
+                RunScriptsCommand(),
+            ]
+        )
+        cli.helpMessageGenerator = ZeroHelpMessageGenerator()
+        return cli
+    }()
+}
+
+struct ZeroHelpMessageGenerator: HelpMessageGenerator {
+    func writeErrorLine(for message: String, to out: WritableStream) {
+        out <<< TTY.error(message)
+    }
 }
