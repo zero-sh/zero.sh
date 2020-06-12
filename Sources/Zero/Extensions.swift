ca25adb7b4effbdde19a54f373c9c922324a8c65
Michael Sanders <michael.sanders@fastmail.com>
2020-06-12 09:23:04 -0700

    Add documentation for extensions

--- Sources/Zero/Extensions.swift
+++ Sources/Zero/Extensions.swift
@@ -20,12 +20,16 @@ extension Path {
 }
 
 extension Path: ConvertibleFromString {
+    /// Instantiate path from the given string. Unless given an absolute path,
+    /// assumes a relative path within current working directory.
     public init?(input: String) {
         self = Path(input) ?? Path.cwd.join(input)
     }
 }
 
 extension Workspace: ConvertibleFromString {
+    /// Instantiate a workspace from the given string with "." as a delimiter,
+    /// e.g. "home.laptop" will become `["home", "laptop"]`.
     public init?(input: String) {
         self = input.split(separator: ".").map(String.init)
     }
