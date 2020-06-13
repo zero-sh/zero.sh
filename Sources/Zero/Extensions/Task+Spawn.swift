28659658c0ca7c66c943c7c631460189a663d9fd
Michael Sanders <michael.sanders@fastmail.com>
2020-06-12 23:25:55 -0700

    Improve error handling when scripts aren't executable
    Fixes #6.

--- Sources/Zero/Extensions/Task+Spawn.swift
+++ Sources/Zero/Extensions/Task+Spawn.swift
@@ -24,12 +24,27 @@ extension SpawnError {
 ///   - https://gist.github.com/dduan/d4e967f3fc2801d3736b726cd34446bc
 extension Task {
     static func spawn(_ executable: String, arguments: [String] = []) throws -> Int32 {
+        guard let absolutePath = Path(executable), absolutePath.exists else {
+            throw SpawnError(exitStatus: 127, message: "\(executable): No such file or directory")
+        }
+        guard absolutePath.isExecutable else {
+            throw SpawnError(exitStatus: 126, message: "\(executable): Permission denied")
+        }
+
         var exitStatus: Int32 = 0
         try withCStrings(arguments) { cArgs in
             let envs = ProcessInfo().environment.map { key, value in "\(key)=\(value)" }
             try withCStrings(envs) { cEnvs in
                 var pid: pid_t = 0
                 let spawnStatus = posix_spawn(&pid, executable, nil, nil, cArgs, cEnvs)
+                if spawnStatus != 0, errno == ENOENT {
+                    throw SpawnError(
+                        exitStatus: 125,
+                        message: "The file '\(executable)' is marked as an executable but could " +
+                            "not be read by the operating system."
+                    )
+                }
+
                 guard spawnStatus == 0, waitpid(pid, &exitStatus, 0) != -1 else {
                     throw NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: [:])
                 }
