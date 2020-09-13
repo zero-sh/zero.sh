d1e6f711f5db21f2b36284ebebc60f8cea2e2a67
Michael Sanders <michael.sanders@fastmail.com>
2020-09-12 22:48:07 -0700

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ package.products = [
     .executable(name: "zero", targets: ["main"]),
 ]
 package.dependencies = [
-    .package(url: "https://github.com/mxcl/Path.swift.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/mxcl/Path.swift.git", .upToNextMajor(from: "1.2.0")),
     .package(url: "https://github.com/onevcat/Rainbow.git", .upToNextMajor(from: "3.1.5")),
     .package(url: "https://github.com/jakeheis/SwiftCLI.git", .upToNextMajor(from: "6.0.2")),
 ]
